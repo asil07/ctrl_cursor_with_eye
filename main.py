1186d577e696b16af7af4ef2ccd12f0db7163924
asilmuhammadiy <asilbekyuldashev07@gmail.com>
2022-10-18 17:33:45 +0900

    cursor control with cv2

---
+++ main.py
@@ -0,0 +1,38 @@
+import cv2
+import mediapipe as mp
+import pyautogui
+
+cam = cv2.VideoCapture(0)
+
+face_mesh = mp.solutions.face_mesh.FaceMesh(refine_landmarks=True) # to get eye landmarks
+                                                                    # refine_landmarks true
+# to get screen size
+screen_w, screen_h = pyautogui.size()
+
+while True:
+    grabbed, frame = cam.read()
+    frame = cv2.flip(frame, 1)
+    rgb_img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    output = face_mesh.process(rgb_img)
+    landmark_points = output.multi_face_landmarks
+    frame_h, frame_w, _ = frame.shape
+    if landmark_points:
+        landmarks = landmark_points[0].landmark
+
+        # looping through to draw circle on detected face landmarks
+        for id, landmark in enumerate(landmarks[474:478]):  # 474 : 478 landmarks for eye
+            x = int(landmark.x * frame_w)
+            y = int(landmark.y * frame_h)
+
+            cv2.circle(frame, (x, y), 3, (0, 255, 0))
+            # to move cursor
+            if id == 1:
+                # to get proper ratio of screen and mouse
+                screen_x = int(landmark.x * screen_w)
+                screen_y = int(landmark.y * screen_h)
+                pyautogui.moveTo(screen_x, screen_y)
+
+    cv2.imshow("EYe controller", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
